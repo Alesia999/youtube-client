51dbbf13c3792bbc0d662513ce296934ac22e5e8
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2022-08-03 22:14:54 +0200

    feat: add toggling functionality

--- youtube-client/src/app/header/header/header.component.ts
+++ youtube-client/src/app/header/header/header.component.ts
@@ -1,28 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   searchText!: string;
   sortingText!: string;
   isFilterOn: boolean = false;
-  constructor() { }
+  @Output() isResultOn: EventEmitter<boolean> = new EventEmitter();
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   search(searchText: string) {
-		if (searchText) {
-			console.log(searchText)
-		}
-	}
+    if (searchText) {
+      console.log(searchText);
+      this.isResultOn.emit(true);
+    }else{
+      this.isResultOn.emit(false);
+    }
+  }
   sortBySortingText(sortingText: string) {
-		if (sortingText) {
-			console.log(sortingText)
-		}
-	}
+    if (sortingText) {
+      console.log(sortingText);
+    }
+  }
   toggleFilter() {
     this.isFilterOn = !this.isFilterOn;
   }
