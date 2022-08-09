e7c7eb74cd62a893a083f0a7d9f28904882da463
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2022-08-09 15:19:35 +0200

    feat: add detailed page

--- src/app/youtube/components/search-item-detailed/search-item-detailed.component.ts
+++ src/app/youtube/components/search-item-detailed/search-item-detailed.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, Observable } from 'rxjs';
 import { SearchItem } from 'src/app/youtube/models/search-item.model';
-import { SearchResults } from '../../models/search-results.model';
-import { YoutubeService } from '../../services/youtube.service';
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-search-item-detailed',
@@ -16,7 +14,7 @@ export class SearchItemDetailedComponent implements OnInit {
 
   constructor(
     private readonly route: ActivatedRoute,
-    
+    private location: Location
   ) {}
 
   ngOnInit(): void {
@@ -26,5 +24,18 @@ export class SearchItemDetailedComponent implements OnInit {
     this.route.data.subscribe(
       (searchItem) => (this.searchItem = searchItem['searchItemDetailed'])
     );
+  }
+
+  dateDiffInDays(item: SearchItem) {
+    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+    const a = new Date(Date.now());
+    const b = new Date(item.date);
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+    return Math.floor((utc1 - utc2) / _MS_PER_DAY);
+  }
+
+  goBack() {
+    this.location.back();
   }
 }
