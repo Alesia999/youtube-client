d18bb81528f32c363de9a767df2822cc24df0512
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2022-08-18 13:15:19 +0200

    feat: add interceptor

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { RouterModule } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './services/auth.interceptor';
+
+const INTERCEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: AuthInterceptor,
+  multi: true,
+};
 
 @NgModule({
   declarations: [LoginComponent, NotFoundComponent],
@@ -15,6 +22,6 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     RouterModule.forChild([{ path: '', component: LoginComponent }]),
   ],
   exports: [LoginComponent, RouterModule],
-  providers: [AuthService],
+  providers: [AuthService, INTERCEPTOR_PROVIDER],
 })
 export class AuthModule {}
