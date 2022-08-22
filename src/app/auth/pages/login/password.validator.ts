45b9741f4daa5c619bb2c10a39eaa190446fd25e
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2022-08-22 17:59:28 +0200

    feat: add custom validator

---
+++ src/app/auth/pages/login/password.validator.ts
@@ -0,0 +1,41 @@
+import { FormControl } from '@angular/forms';
+
+export class PasswordValidator {
+  static minLength(control: FormControl): { [key: string]: boolean } | null {
+    if (control.value.length < 8) {
+      return {
+        minLength: true,
+      };
+    }
+    return null;
+  }
+
+  static upperCase(control: FormControl): { [key: string]: boolean } | null {
+    if (!/[a-z]/.test(control.value) || !/[A-Z]/.test(control.value)) {
+      return {
+        upperCase: true,
+      };
+    }
+    return null;
+  }
+
+  static numbers(control: FormControl): { [key: string]: boolean } | null {
+    if (!/?=.*[0-9]/.test(control.value)) {
+      return {
+        numbers: true,
+      };
+    }
+    return null;
+  }
+
+  static specialCharacter(
+    control: FormControl
+  ): { [key: string]: boolean } | null {
+    if (!/?=.*[!@#$%^&*]/.test(control.value)) {
+      return {
+        specialCharacter: true,
+      };
+    }
+    return null;
+  }
+}
