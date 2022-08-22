93747468257e3189235898b61758958c45ec2bf5
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2022-08-22 20:15:55 +0200

    feat: add validators to admin page

--- src/app/auth/pages/admin/admin.component.ts
+++ src/app/auth/pages/admin/admin.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DateValidator } from './date.validator';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent {
+  adminForm = new FormGroup({
+    title: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    description: new FormControl('', [Validators.maxLength(255)]),
+    image: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'
+      ),
+    ]),
+    video: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'
+      ),
+    ]),
+    date: new FormControl('', [Validators.required, DateValidator.ptDate]),
+  });
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submit() {
+    if (this.adminForm.valid) {
+      this.adminForm.reset();
+      this.router.navigateByUrl('/main');
+    } else {
+      Object.keys(this.adminForm.controls).forEach((key: string) => {
+        const abstractControl = this.adminForm.controls[key];
+        abstractControl.markAsTouched();
+      });
+    }
   }
-
 }
