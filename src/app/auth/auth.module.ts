50f863ef78b677ca1432095c5913f3f43abba001
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2022-08-22 10:29:27 +0200

    fix: code

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,18 +1,10 @@
-import { NgModule, Provider } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './services/auth.interceptor';
-
-const INTERCEPTOR_PROVIDER: Provider = {
-  provide: HTTP_INTERCEPTORS,
-  useClass: AuthInterceptor,
-  multi: true,
-};
 
 @NgModule({
   declarations: [LoginComponent, NotFoundComponent],
@@ -22,6 +14,6 @@ const INTERCEPTOR_PROVIDER: Provider = {
     RouterModule.forChild([{ path: '', component: LoginComponent }]),
   ],
   exports: [LoginComponent, RouterModule],
-  providers: [AuthService, INTERCEPTOR_PROVIDER],
+  providers: [AuthService],
 })
 export class AuthModule {}
